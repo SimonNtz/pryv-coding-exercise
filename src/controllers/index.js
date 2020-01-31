fc8f1202df19f776a547e13091b63d81f3be06b7
simon <simon.ntz@gmail.com>
2020-01-31 16:41:42 +0100

    finish ex2

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -5,12 +5,14 @@
 
 // Required services
 const { fetchStreams, saveEvent } = require('../services');
+const loadash = require('lodash');
 
 // This function is chaining two asynchronous calls to services.
 const saveStreamstoEvent = async function (req, res) {
     const { source, backup } = req.body;
     try {
         const streamsResp = await fetchStreams(source, backup);
+        intersectStreams(streamsResp[0].streams[0], streamsResp[1].streams[0]);
         const eventResp = await saveEvent(backup, 
             streamsResp[0].streams.concat(streamsResp[1].streams));
         res.json(eventResp);
@@ -19,6 +21,57 @@ const saveStreamstoEvent = async function (req, res) {
         res.sendStatus(500);
     }
 };
+
+
+function intersectStreams(s1, s2){
+    // console.log(s1,s2);
+    var linkArray = [];
+    var linkArray2 = [];
+    var intersectFin = [];
+
+    function flatTree(tree, arr){
+        arr.push(tree);
+        if (tree.children){
+            for (var i=0; i < tree.children.length; i++){
+                flatTree(tree.children[i], arr);
+            }
+        }            
+    }
+
+    flatTree(s1, linkArray);
+    flatTree(s2, linkArray2);
+
+    for (var i=0; i < linkArray.length ; i++) {
+        for (var j=0; j < linkArray2.length ; j++){
+            if ((linkArray[i].id == linkArray2[j].id) && (linkArray[i].parentId == linkArray2[j].parentId)) {
+                intersectFin.push(linkArray[i].id);
+            }
+        }
+    }
+
+    var intersecTree = {};
+
+    function constructTree(tree, _intersect){
+        if (_intersect.includes(tree.id)){
+            var treeClone = loadash.cloneDeep(tree);
+            treeClone.children = null;
+            // ``if (!intersecTree)`` doesn't work
+            if (Object.keys(intersecTree) == 0) {
+                intersecTree = treeClone;
+            } else {
+                intersecTree['children'] = treeClone;
+            }
+        }
+
+        for (var i=0; i < tree.children.length; i++){
+            constructTree(tree.children[i], _intersect);
+        }          
+    }
+    
+    constructTree(s1, intersectFin);
+
+    console.log(intersecTree);
+}
 
 module.exports = {
     saveStreamstoEvent
---
+++ src/utils/index.js
@@ -0,0 +1,3 @@
+function intersectStreams(streams){
+
+};
